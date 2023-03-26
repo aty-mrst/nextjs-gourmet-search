2bd929631be956516fd6f78dc3c7db0975655c36
AtsuyaMorishita <mori.a0426@gmail.com>
2023-03-26 20:34:26 +0900

    地域選択はヘッダーにまとめる

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,20 @@
+import { PLACE } from "@/data/data";
 import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="h-[60px] leading-[60px] text-center">
-      <h1 className="inline-block">
-        <Link href={"/"}>山口県 グルメガイド</Link>
+    <header className="text-center py-5 border-b fixed w-[100%] top-0 bg-white z-20">
+      <h1 className="inline-block text-xl">
+        <Link href={"/"}>〜山口県 グルメガイド〜</Link>
       </h1>
+
+      <div className="whitespace-nowrap overflow-x-scroll">
+        {PLACE.map((place) => (
+          <Link key={place.NAME} href={place.URL} className="mx-3">
+            {place.NAME}
+          </Link>
+        ))}
+      </div>
     </header>
   );
 };
