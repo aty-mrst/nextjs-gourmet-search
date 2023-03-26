69f60478e85f731086e869cb16e0037a1762f5e9
AtsuyaMorishita <mori.a0426@gmail.com>
2023-03-26 20:36:29 +0900

    データのファイル名変更、各種追加

---
+++ src/data/data.ts
@@ -0,0 +1,65 @@
+export const PLACE = [
+  { KEY: "all", URL: "/", NAME: "山口県全域", POSITION: "&service_area=SA75" },
+  {
+    KEY: "yudaonsen",
+    URL: "/area/yudaonsen",
+    NAME: "湯田温泉",
+    POSITION: "&lat=34.16497682781039&lng=131.4571897371615",
+  },
+  {
+    KEY: "hofu",
+    URL: "/area/hofu",
+    NAME: "防府市",
+    POSITION: "&lat=34.054470150656854&lng=131.56869995888923",
+  },
+  {
+    KEY: "iwakuni",
+    URL: "/area/iwakuni",
+    NAME: "岩国市",
+    POSITION: "&lat=34.17057635622233&lng=132.220462531636",
+  },
+  {
+    KEY: "shimonoseki",
+    URL: "/area/shimonoseki",
+    NAME: "下関市",
+    POSITION: "&lat=33.96576082350943&lng=130.94190157495564",
+  },
+  {
+    KEY: "ube",
+    URL: "/area/ube",
+    NAME: "宇部市",
+    POSITION: "&lat=33.956951414684696&lng=131.24490100176504",
+  },
+  {
+    KEY: "nagato",
+    URL: "/area/nagato",
+    NAME: "長門市",
+    POSITION: "&lat=34.374987347107165&lng=131.18097959512923",
+  },
+  {
+    KEY: "hagi",
+    URL: "/area/hagi",
+    NAME: "萩市",
+    POSITION: "&lat=34.416051855007936&lng=131.39850401740074",
+  },
+];
+
+export const GENRES = [
+  { NAME: "居酒屋", NUM: "G001" },
+  { NAME: "ダイニングバー・バル", NUM: "G002" },
+  { NAME: "創作料理", NUM: "G003" },
+  { NAME: "和食", NUM: "G004" },
+  { NAME: "洋食", NUM: "G005" },
+  { NAME: "イタリアン・フレンチ", NUM: "G006" },
+  { NAME: "中華", NUM: "G007" },
+  { NAME: "焼肉・ホルモン", NUM: "G008" },
+  { NAME: "韓国料理", NUM: "G017" },
+  { NAME: "アジア・エスニック料理", NUM: "G009" },
+  { NAME: "各国料理", NUM: "G010" },
+  { NAME: "カラオケ・パーティ", NUM: "G011" },
+  { NAME: "バー・カクテル", NUM: "G012" },
+  { NAME: "ラーメン", NUM: "G013" },
+  { NAME: "お好み焼き・もんじゃ", NUM: "G016" },
+  { NAME: "カフェ・スイーツ", NUM: "G014" },
+  { NAME: "その他グルメ", NUM: "G015" },
+];
