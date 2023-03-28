3f2800a595c99064c48099eab7f8226078cbcf92
AtsuyaMorishita <mori.a0426@gmail.com>
2023-03-28 09:37:36 +0900

    ページトップ戻るボタン追加 pcのみ

--- src/components/FixedButton.tsx
+++ src/components/FixedButton.tsx
@@ -3,15 +3,32 @@ type FixedButtonType = {
 };
 
 const FixedButton = ({ onClick }: FixedButtonType) => {
+  const scrollTo = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <button
-      className="fixed right-[15px] bottom-[15px] border rounded-[50%] w-[80px] h-[80px] inline-block text-sm lg:hidden z-30 bg-white"
-      onClick={onClick}
-    >
-      ジャンルを
-      <br />
-      選択
-    </button>
+    <>
+      {/* sp */}
+      <button
+        className="fixed right-[15px] bottom-[15px] border rounded-[50%] w-[80px] h-[80px] inline-block text-sm lg:hidden z-30 bg-white"
+        onClick={onClick}
+      >
+        ジャンルを
+        <br />
+        選択
+      </button>
+
+      {/* pc */}
+      <button
+        className="hidden lg:inline-block fixed right-[15px] bottom-[15px] border rounded-[50%] w-[80px] h-[80px] text-sm z-30 bg-white"
+        onClick={scrollTo}
+      >
+        ページ
+        <br />
+        トップ
+      </button>
+    </>
   );
 };
 
