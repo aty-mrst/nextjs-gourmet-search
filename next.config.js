797a217bb33cf757f4fea38dee8f242162b2fb6c
AtsuyaMorishita <mori.a0426@gmail.com>
2023-03-30 21:46:28 +0900

    .

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/middleware-manifest.json$/],
 });
 
 module.exports = withPWA({
