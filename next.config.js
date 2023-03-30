a20e1061ddb27d946c52d2298e15c53be1a6d5d0
AtsuyaMorishita <mori.a0426@gmail.com>
2023-03-30 09:07:50 +0900

    next-pwaのバージョンを下げてみた

--- next.config.js
+++ next.config.js
@@ -1,12 +1,31 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  buildExcludes: [/middleware-manifest.json$/],
-});
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+// });
 
-module.exports = withPWA({
+// module.exports = withPWA({
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "imgfp.hotp.jp",
+//         port: "",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+// });
+
+const withPWA = require("next-pwa");
+
+const nextConfig = withPWA({
+  pwa: {
+    dest: "public",
+  },
   reactStrictMode: true,
+  trailingSlash: true,
   images: {
     remotePatterns: [
       {
@@ -18,3 +37,5 @@ module.exports = withPWA({
     ],
   },
 });
+
+module.exports = nextConfig;
