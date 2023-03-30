236ffbdd469a57ed50a30cdecd4741d4b3030f0f
AtsuyaMorishita <mori.a0426@gmail.com>
2023-03-30 09:16:32 +0900

    pwa調整

--- next.config.js
+++ next.config.js
@@ -1,31 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-// const withPWA = require("next-pwa")({
-//   dest: "public",
-// });
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-// module.exports = withPWA({
-//   reactStrictMode: true,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "imgfp.hotp.jp",
-//         port: "",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-// });
-
-const withPWA = require("next-pwa");
-
-const nextConfig = withPWA({
-  pwa: {
-    dest: "public",
-  },
+module.exports = withPWA({
   reactStrictMode: true,
-  trailingSlash: true,
   images: {
     remotePatterns: [
       {
@@ -37,5 +20,3 @@ const nextConfig = withPWA({
     ],
   },
 });
-
-module.exports = nextConfig;
