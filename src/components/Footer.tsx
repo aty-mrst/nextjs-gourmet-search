f5c4aa8fa2ad906321e223a534ab5477d6e5b023
AtsuyaMorishita <mori.a0426@gmail.com>
2023-04-08 16:13:56 +0900

    スタイル調整

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="h-[60px] leading-[60px] text-center text-sm bg-[#FEF6E8] mt-10 border border-[#F8E6CC]">
+    <footer className="h-[60px] leading-[60px] text-center text-sm bg-[#FEF6E8] mt-20 border border-[#F8E6CC]">
       Powered by{" "}
       <a
         href="http://webservice.recruit.co.jp/"
